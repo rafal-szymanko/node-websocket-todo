5dc3e8995d79321a03f544056fc6f96e52da1213
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-08-15 18:30:47 +0200

    Add listeners for addTask&removeTask&updateData events

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import io from 'socket.io-client';
+
+
+const socket = io('localhost:8000');
 
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState('');
+
+
+  useEffect(() => {
+
+    socket.on('addTask', ({
+      task,
+    }) => addTask(task));
+
+    socket.on('removeTask', ({
+      id,
+    }) => removeTask(id));
+
+    socket.on('updateData', (data) => updateTask(data));
+  }, );
+
+  const removeTask = (id) => {
+    const removedTask = tasks[id];
+    setTasks(tasks.filter(item => item != removedTask));
+
+    socket.emit('removeTask', {
+      id: id,
+    });
+  };
+
+  const updateTask = (serverData) => {
+    setTasks([...tasks, ...serverData.tasks]);
+  }
+
+
+  const submitForm = (event) => {
+    event.preventDefault();
+
+    if (taskName) {
+      addTask(taskName);
+      socket.emit('addTask', {
+        task: taskName,
+      });
+    } else {
+      alert('Make sure you have write your task');
+    };
+
+    event.target.reset();
+    setTaskName('');
+  };
+
+  const addTask = (task) => {
+    const index = tasks.findIndex(item => item == task);
+    if (index == -1) {
+      setTasks([...tasks, task]);
+    } else {
+      alert('You have already added this task')
+    }
+  }
+
   return (
     <div className="App">
-
     <header>
       <h1>ToDoList.app</h1>
     </header>
-
     <section className="tasks-section" id="tasks-section">
       <h2>Tasks</h2>
-
       <ul className="tasks-section__list" id="tasks-list">
-        <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-        <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+        {tasks.map(item => <li className="task" key={tasks.findIndex(task => task == item)}>{item}<button className="btn btn--red" onClick={()=> removeTask(tasks.findIndex(task => task == item))}>Remove</button></li>)}
       </ul>
-
-      <form id="add-task-form">
-        <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
+      <form id="add-task-form" onSubmit={submitForm}>
+        <input className="text-input" autoComplete="off" type="text" placeholder="Type your description" id="task-name" onChange={event => setTaskName(event.target.value)}/>
         <button className="btn" type="submit">Add</button>
       </form>
-
     </section>
   </div>
   );
--- server.js
+++ server.js
@@ -2,8 +2,29 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const path = require('path');
+const socket = require('socket.io');
 
+const tasks = ['Shopping', 'Go out with a dog'];
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
+
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    socket.emit('updateData', {tasks});
+  socket.on('addTask', (newTask) => {
+    tasks.push(newTask.task);
+    socket.broadcast.emit('addTask', newTask);
+    console.log(tasks);
+  });
+
+  socket.on('removeTask', (id) => {
+    tasks.splice(id, 1);
+    socket.emit('removeTask', id);
+    console.log(id);
+  });
+  socket.on('disconnect', () => {});
+});
